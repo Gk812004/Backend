d4d89d5b346e59ca7af97b7d70db1169671d4945
Gaurav Kumar <gk812004@gmail.com>
2024-03-12 01:20:07 +0530

    utils discuss

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express  from "express";
+import cors from"cors"; // it will connect fronted to the Backend 
+import cookieParser from "cookie-parser"; // it will help to store the cookie in the local system
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,  // it will get the data from env file where it will help to connect the domain
+    credentials: true
+}))
+
+app.use(express.json({limit:"16kb"})) // limit the json file 
+
+app.use(express.urlencoded({
+    extended : true,
+    limit:"16kb"
+}))  // it will auto adjust  the url 
+app.use(express.static("public"))// create a Public file where it will store photo/picture
+
+app.use(cookieParser())
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ dotenv.config({
 })
 
 connectDB()
+
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+    app.on("error",(error)=>{
+        console.log("error: server is not running",error);
+        throw error
+    })
+})
+.catch((err) =>{
+    console.log("mogo db COnnection faiked !!!", err);
+})
